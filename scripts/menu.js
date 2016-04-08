c02d77388c4c40c78ff726f3dea935883fcadd14
Spencer Smith <ssmith@egi.utah.edu>
2016-04-08 16:47:13 -0600

    stop propagation

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,12 @@
 $('.links').load("menu.html");
 
+$('.links a').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log($(this).text());
+    $(".content").load($(this).attr("href"));
+});
+
 window.setTimeout(function(){
   $('.links').css("opacity", "1");
 }, 400);
-
-$('.links a').click(function(e) {
-    e.preventDefault();
-    $(".content").load($(this).attr("href"));
-});
