7bb45f75aa734238fdbb37d9b35ca612dd150985
Spencer Smith <ssmith@egi.utah.edu>
2016-06-02 12:45:31 -0600

    Updated Mailchimp stat-viewer to allow for changes in data field locations

--- _tools/mailchimp/script.js
+++ _tools/mailchimp/script.js
@@ -1,4 +1,5 @@
 var finput;
+var data;
 
 window.onload = function(){
   main()
@@ -26,25 +27,43 @@ function main(){
 
   function dataLoaded(results){
     var body = document.body;
-    var data = results.data;
+    data = results.data;
+    dataLastIndex = data.length - 2;
     hide(finput);
+
+    var fields = [
+      "Total Recipients:",
+      "Recipients Who Opened:",
+      "Total Clicks:",
+      "Total Unsubs:",
+      "Bounces:",
+      "Total Unsubs:"
+      ];
+
     body.innerHTML = "<button onclick='location.reload()'>Restart</button>" + body.innerHTML
 
     body.innerHTML += "<h2>" + data[1][1] + "</h2>"; // Title
     //body.innerHTML += "<p>" + data[3][1] + "</p>";   // Date
     body.innerHTML += "<h3>Mailchimp Statistics</h3>";
-    body.innerHTML += "<p><span class='first'>" + data[6][0] + "</span>" + data[6][1] + "</p>";   // Total Recipents
-    // body.innerHTML += "<p><span class='first'>" + data[12][0] + "</span>" + data[12][1] + "</p>"; // Total Opens
-    body.innerHTML += "<p><span class='first'>" + "Opens:" + "</span>" + data[11][1] + "</p>";    // Unique Opens
-    body.innerHTML += "<p><span class='first'>" + data[15][0] + "</span>" + data[15][1] + "</p>"; // Total Clicks
-    body.innerHTML += "<p><span class='first'>" + data[8][0] + "</span>" + data[8][1] + "</p>";   // Bounced
-    body.innerHTML += "<p><span class='first'>" + data[17][0] + "</span>" + data[17][1] + "</p>"; // Unsubscriptions
+
+    // Prints all fields to document body
+    printFields(fields);
 
     body.innerHTML += "<h3 class='article'>Top Articles</h3>";
     body.innerHTML += "<div class='article-wrap'></div>";
+
     var articleWrap = document.getElementsByClassName('article-wrap')[0];
     articleWrap.innerHTML += "<span class='clicks'><span class='article' style='padding-right: 0;'></span>Clicks</span>";
-    for(i = 23; i <= 27; i++){
+
+    var articleStart = getArticleStart();
+    var lastOfTop = articleStart + 5;
+
+    // Sets last of top to
+    if(lastOfTop > dataLastIndex){
+      lastOfTop = dataLastIndex;
+    }
+
+    for(i = articleStart; i <= lastOfTop; i++){
       articleWrap.innerHTML += "<p><span class='article'>" + data[i][0].replace(/\//g, " ").trim().split(" ").pop() + "</span>" + data[i][1] + "</p>";
     }
 
@@ -52,8 +71,30 @@ function main(){
     body.innerHTML += "<div class='article-wrap'></div>";
     articleWrap = document.getElementsByClassName('article-wrap')[1];
     articleWrap.innerHTML += "<span class='clicks'><span class='article' style='padding-right: 0;'></span>Clicks</span>";
-    for(i = 23; i <= data.length - 2; i++){
+
+    for(i = articleStart; i <= dataLastIndex; i++){
       articleWrap.innerHTML += "<p><span class='article'>" + data[i][0].replace(/\//g, " ").trim().split(" ").pop() + "</span>" + data[i][1] + "</p>";
+    }
+  }
+
+  function printFields(fields){
+    for(var i = 0; i < fields.length; i++){
+      document.body.innerHTML += "<p><span class='first'>" + fields[i] + "</span>" + getValue(fields[i]) + "</p>"; // Unsubscriptions
+    }
+  }
+  function getValue(field){
+    for(var i = 0; i < data.length; i++){
+      if(data[i][0] == field){
+        return data[i][1];
+      }
+    }
+  }
+
+  function getArticleStart(){
+    for(var i = 0; i < data.length; i++){
+      if(data[i][0] == "URL"){
+        return i + 1;
+      }
     }
   }
 
